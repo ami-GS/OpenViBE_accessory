8bbf9861f74edf2c169cfeb45bce0fb896e8848f
ami-GS <1991.daiki@gmail.com>
2014-06-09 10:52:25 +0900

    made option of reading from file in each trial or on memory

--- playSound_udp.py
+++ playSound_udp.py
@@ -9,7 +9,7 @@ CHUNK = 4096
 #stimuliName = ["tm2_switch000.wav", "tm2_switch001.wav",
 #               "tm2_swing002.wav", "tm2_swing004.wav"]
 stimuliName = ["audio/lu.wav", "audio/ru.wav", "audio/rd.wav", "audio/ld.wav"]
-
+content = []
 
 for i in range(4):
     wf.append(wave.open(stimuliName[i], "rb"))
@@ -18,26 +18,44 @@ for i in range(4):
                          rate = wf[i].getframerate(),
                          output=True))
 
-def play(fNum):
+def readContent():
+    for i in range(len(stimuliName)):
+        data = wf[i].readframes(CHUNK)
+        tmp = []
+        while data:
+            tmp.append(data)
+            data = wf[i].readframes(CHUNK)
+        content.append(tmp)
+
+def playOnMemory(fNum):
+    for data in content[fNum]:
+        stream[fNum].write(data)
+
+
+def playFromFile(fNum):
     data = wf[fNum].readframes(CHUNK)
     while data:
         stream[fNum].write(data)
         data = wf[fNum].readframes(CHUNK)
     else:
         wf[fNum] = wave.open(stimuliName[fNum], "rb")
-        
-    
 
 if __name__ == "__main__":
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     udp.bind(("127.0.0.1", 12345))
+    play = playOnMemory # read file only once
+    #play = playFromFile # read file in each call
+    readContent()
     while True:
         fNum, addr = udp.recvfrom(128)
         if fNum == "finish":
             break
         else:
             play(int(fNum))
-
+    
+    for i in range(len(stimuliName)):
+        wf[i].close()
+        stream[i].close()
     udp.close()
         
