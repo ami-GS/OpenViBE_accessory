25d624c050632c323f8d3ff08e770924e5b92598
Daiki Aminaka <1991.daiki@gmail.com>
2014-06-11 22:23:43 +0900

    fix typo and added finish procedure

--- player.py
+++ player.py
@@ -30,7 +30,7 @@ class MyOVBox(OVBox):
         self.stimLabels = ["OVTK_StimulationId_Label_0"+str(i) for i in range(self.stimNum+1)]
         self.trial = int(self.setting["trialsPerStimulus"])
         self.untilStart = int(self.setting["untilStart"])
-        self.allTrial = self.Num * self.trial
+        self.allTrial = self.stimNum * self.trial
         seqFileName = self.setting["Random sequence file path"] #path
         self.sequence = self.randomSequence()
         with open(seqFileName, "w") as f:
@@ -72,21 +72,31 @@ class MyOVBox(OVBox):
         
     def process(self):
         if self.getCurrentTime() >= self.untilStart:
-            if self.count and self.count % self.duration == 0:
-                stimuNum = self.sequence[self.cursor]
-                self.generateStimulation(stimuNum)
+            if self.cursor >= self.allTrial+1:
+                print self.cursor, "fin"
+                stimSet = OVStimulationSet(self.getCurrentTime(), self.getCurrentTime()+1./self.getClock())
+                stimSet.append(OVStimulation(OpenViBE_stimulation["OVTK_StimulationId_Label_05"], self.getCurrentTime(), 0.))
+                self.output[0].append(stimSet)
+
+            elif self.count and self.count % self.duration == 0:
+                print self.cursor
+                try:
+                    stimuNum = self.sequence[self.cursor]
+                    self.generateStimulation(stimuNum)
+                    self.cursor += 1
+                    self.count = 0
+                    self.final = self.doSubCount
+                    #temporaly, this uses subprocess.Popen to reduce (maybe visually) latency
+                    udp.sendto(str(stimuNum-1), ("127.0.0.1", 12345))
+                except:
+                    pass
                 self.cursor += 1
-                self.count = 0
-                self.final = self.doSubCount
-                #temporaly, this uses subprocess.Popen to reduce (maybe visually) latency
-                udp.sendto(str(stimuNum-1), ("127.0.0.1", 12345))
             elif self.subCount == self.interval:
                 #redundant stimulus to makesure the start of new tartget
                 self.generateStimulation(0) 
                 self.final = self.doCount
                 self.subCount = 0
-        elif self.cursor >= self.allTeial:
-            self.output[1].append("Some stimuli to stop processing.") #TODO
+  
         self.final()
 
 box = MyOVBox()
